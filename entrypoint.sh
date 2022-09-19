7f2fddc7634607dc6adfea85c033e4b3a7358b0f
Tonye Jack <jtonye@ymail.com>
2022-09-19 13:05:20 -0600

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
+
+if [[ $GITHUB_REF != "refs/tags/"* ]]; then
+  echo "::warning::Skipping: This should only run on tags push or on release instead of '$GITHUB_EVENT_NAME'.";
+  exit 0;
+fi
+
+git fetch origin +refs/tags/*:refs/tags/*
+
+NEW_TAG=${GITHUB_REF/refs\/tags\//}
+MAJOR_VERSION=$(echo "$NEW_TAG" | cut -d. -f1)
+
+# Re-tag the major version using the latest tag
+git tag -f "$MAJOR_VERSION" "$NEW_TAG"
+git push -f "$INPUTS_REMOTE" "$MAJOR_VERSION"
+
+echo "::set-output name=major_version::$MAJOR_VERSION"
+echo "::set-output name=new_version::$NEW_TAG"
