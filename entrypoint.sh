b05b10dccb2e4e5d6f1274cca503a0d6242acc7a
Tonye Jack <jtonye@ymail.com>
2022-09-19 13:14:21 -0600

    Updated warning message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ $GITHUB_REF != "refs/tags/"* ]]; then
-  echo "::warning::Skipping: This should only run on tags push or on release instead of '$GITHUB_EVENT_NAME'.";
+  echo "::warning::Skipping: This should only run on tags push or on release.";
   exit 0;
 fi
 
