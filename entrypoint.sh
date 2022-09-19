07ecfdd77341dce1b485295e5fc754ebf06726fc
Tonye Jack <jtonye@ymail.com>
2022-09-19 14:04:12 -0600

    Updated action.yml and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,19 @@ git fetch origin +refs/tags/*:refs/tags/*
 NEW_TAG=${GITHUB_REF/refs\/tags\//}
 MAJOR_VERSION=$(echo "$NEW_TAG" | cut -d. -f1)
 
-# Re-tag the major version using the latest tag
 git tag -f "$MAJOR_VERSION" "$NEW_TAG"
 git push -f "$INPUTS_REMOTE" "$MAJOR_VERSION"
+
+for tag in $(git tag -l "$MAJOR_VERSION.*"); do
+  echo "Adding $tag to release notes"
+  {
+    echo "# Changes in $tag"
+    gh release view "$tag" --json body --jq '.body'
+    printf "\n---\n\n"
+  } >> "$INPUTS_RELEASE_NOTES_FILE"
+done
+
+gh release edit "$MAJOR_VERSION" --notes-file "$INPUTS_RELEASE_NOTES_FILE"
 
 echo "::set-output name=major_version::$MAJOR_VERSION"
 echo "::set-output name=new_version::$NEW_TAG"
