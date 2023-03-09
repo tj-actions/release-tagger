26b76447213510a1c78df9fd6cbf0354c5f13c1b
Tonye Jack <jtonye@ymail.com>
2023-03-09 02:31:46 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,7 +40,7 @@ if [[ -f "$INPUTS_RELEASE_NOTES_FILE" ]]; then
     git tag -f "$MAJOR_VERSION" "$NEW_TAG"
     git push -f "$INPUTS_REMOTE" "$MAJOR_VERSION"
   else
-    if [ -z "$(ls -A "$RELEASE_ASSETS_DIR")" ]; then
+    if [[ -n "$(find "$RELEASE_ASSETS_DIR" -type f)" ]]; then
       gh release create "$MAJOR_VERSION" --notes-file "$INPUTS_RELEASE_NOTES_FILE" --title "$MAJOR_VERSION" "$RELEASE_ASSETS_DIR"/*
     else
       gh release create "$MAJOR_VERSION" --notes-file "$INPUTS_RELEASE_NOTES_FILE" --title "$MAJOR_VERSION"
